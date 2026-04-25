5add698cc86b1d1c9792b91f9b2d51ab4087b4e2
HariniPamukuntla <harinipamukuntla@gmail.com>
2026-04-26 01:24:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -48,8 +48,3 @@ def reject(id):
     return redirect("/incoming")
 
 
-# IMPORTANT FOR RENDER
-if __name__ == "__main__":
-    import os
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
